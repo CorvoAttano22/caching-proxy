3f0d892585e8f7cf2d622c7204b7175cd4709eb4
Corvo <sajjad.dez14@gmail.com>
2026-08-12 18:52:00 +0330

    worked with req and url methods of node to see a request properties

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,18 @@
-import {createServer} from "node:http";
+import { createServer } from "node:http";
+
+const origin = "https://dummyjson.com";
 
 const server = createServer((req, res) => {
-    res.end("Hello from caching proxy")
-});
+  console.log(req.method);
+  console.log(req.url);
 
-server.listen(3000, ()=>{
-    console.log("Server is running on port 3000")
-})
+  const myURL = new URL(req.url ?? "/", origin);
+  console.log(myURL.href);
+  console.log(myURL.origin);
+  console.log(myURL.pathname);
+
+  res.end("Hello from caching proxy");
+});
+server.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
