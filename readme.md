9775954597202829560fa585307295b7b7a9521d
Corvo <sajjad.dez14@gmail.com>
2026-08-23 19:36:31 +0330

    create a map of the response and extracted the needed properties of the headers

---
+++ readme.md
@@ -0,0 +1,18 @@
+# caching-proxy
+
+A tool made to cache the responses sent by the server.
+
+## Installation
+
+$ npm install
+
+## Usage
+
+# build
+$ npm run build
+
+# run
+$ npm run start
+
+# Git Repo
+https://github.com/CorvoAttano22/caching-proxy
--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,70 @@
 import { createServer } from "node:http";
-
+console.log(process.argv);
 const origin = "https://dummyjson.com";
+const cache = new Map<string, CachedResponse>();
+const headersToKeep = ["content-type", "etag", "vary"];
+type CachedResponse = {
+  status: number;
+  headers: Record<string, string>;
+  body: string;
+};
 
-const server = createServer((req, res) => {
-  console.log(req.method);
-  console.log(req.url);
+function createCacheKey(method: string, url: URL): string {
+  return `${method}:${url.href}`;
+}
 
+const server = createServer(async (req, res) => {
   const myURL = new URL(req.url ?? "/", origin);
-  console.log(myURL.href);
-  console.log(myURL.origin);
-  console.log(myURL.pathname);
+  const key = createCacheKey(req.method ?? "GET", myURL);
 
-  res.end("Hello from caching proxy");
+  const cached = cache.get(key);
+
+  //to be understood
+  if (cached) {
+    console.log("Cache Hit");
+
+    res.statusCode = cached.status;
+
+    for (const [key, value] of Object.entries(cached.headers)) {
+      res.setHeader(key, value);
+    }
+
+    res.setHeader("X-Cache", "HIT");
+    res.end(cached.body);
+
+    return;
+  }
+  //
+
+  const response = await fetch(myURL);
+
+  const headers: Record<string, string> = {};
+
+  response.headers.forEach((value, key) => {
+    if (headersToKeep.includes(key)) {
+      headers[key] = value;
+      res.setHeader(key, value);
+    }
+  });
+
+  const body = await response.text();
+
+  res.statusCode = response.status;
+  res.setHeader("X-Cache", "MISS");
+
+  const cachedResponse: CachedResponse = {
+    status: response.status,
+    headers,
+    body,
+  };
+
+  cache.set(key, cachedResponse);
+
+  console.log(key);
+  console.log(cachedResponse);
+  res.end(body);
 });
+
 server.listen(3000, () => {
   console.log("Server is running on port 3000");
 });
